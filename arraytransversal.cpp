97aca051a93ae26609311861478d1692df909f6d
nupurgandhi4120-sys <nupurgandhi4120@gmail.com>
2026-02-24 15:36:28 +0530

    transversal in array

---
+++ arraytransversal.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int array[5]={12, 45, 7, 23, 89};
+    //array traversinf
+    for (int i=0;i<5;i++)
+    {
+        cout<<array[i]<<endl;
+    }
+   }
